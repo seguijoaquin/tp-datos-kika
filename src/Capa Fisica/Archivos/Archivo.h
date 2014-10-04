f0ef3b800e50e147b0339adc04b8afc6254d7f98
franery10@gmail.com <franery10@gmail.com@60b1a8d6-6904-be9c-73d1-5d005d5b9d01>
2014-10-04 22:53:10 +0000

    registro fijo sin mapa bits

--- src/Capa Fisica/Archivos/Archivo.h
+++ src/Capa Fisica/Archivos/Archivo.h
@@ -3,12 +3,46 @@
 
 using namespace std;
 
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include "../../constantes.h"
+
+struct Atributo{
+	int entero;	//Si es una instancia se guarda el ID
+	char* texto;
+};
+
+enum TipoAtributo{
+	ENTERO,
+	TEXTO,
+	ENTID	//entidad
+};
+
+enum TipoArchivo{
+	FIJO,
+	VARIABLE,
+	BLOQUES
+};
+
+struct tamanioYTipoAtributo{
+	TipoAtributo tipo;
+	int cantidadBytes; //cantidad de bytes del tipo de Atributo
+};
+
 class Archivo {
 private:
 
 public:
 	Archivo();
-	~Archivo();
+	virtual ~Archivo();
+
+protected:
+	fstream archivo;
+	string nombreArchivo;
+	virtual list<Atributo>* leer(int numero,list<tamanioYTipoAtributo>* listaTipoAtributos);
+	virtual void escribir(list<Atributo>* atributos, list<tamanioYTipoAtributo>* listaTipoAtributos);
 };
 
 #endif
--- src/Capa Fisica/Archivos/RegistroFijo/RegistroFijo.cpp
+++ src/Capa Fisica/Archivos/RegistroFijo/RegistroFijo.cpp
@@ -1,10 +1,56 @@
 #include "RegistroFijo.h"
 
 
-ArchivoRegistroFijo::ArchivoRegistroFijo(){
-
+ArchivoRegistroFijo::ArchivoRegistroFijo(string nombre){
+	this->archivo.open(nombre.c_str(),ios::in | ios::out | ios::binary);
+	if (!this->archivo) { //Si no existe
+		this->archivo.open(nombre.c_str(),ios::in | ios::out | ios::binary | ios::trunc);
+		this->cantRegistros = 0;
+	} else {
+		this->archivo.seekg(0,ios::beg);
+		this->archivo.read((char*)&this->cantRegistros, sizeof(this->cantRegistros)); //El primer dato que hay en archivo es la cant de registros.
+		this->archivo.read((char*)&this->tamanioRegistros,sizeof(this->tamanioRegistros)); //El segundo dato es el tamanio de cada registro.
+	}
+	this->nombreArchivo = nombre;
 }
 
 ArchivoRegistroFijo::~ArchivoRegistroFijo(){
+	this->archivo.close();
+}
+
+list<Atributo>* ArchivoRegistroFijo::leer(int numeroRegistro, list<tamanioYTipoAtributo>* listaTipoAtributos){
+	list<Atributo>* listaAtributos = new list<Atributo>;
+	this->archivo.seekg(sizeof(this->cantRegistros) + sizeof(this->tamanioRegistros) + numeroRegistro * this->tamanioRegistros,ios::beg);
+
+	for (list<tamanioYTipoAtributo>::iterator it = listaTipoAtributos->begin(); it != listaTipoAtributos->end(); it++) {
+		Atributo aux;
+		if (it->tipo == TEXTO) {
+			this->archivo.read((char*)&aux.texto,it->cantidadBytes);
+		} else {
+			this->archivo.read((char*)&aux.entero,it->cantidadBytes);
+		}
+		listaAtributos->push_back(aux);
+	}
+	return listaAtributos;
+}
+
+void ArchivoRegistroFijo::escribir(list<Atributo>* atributos,list<tamanioYTipoAtributo>* listaTipoAtributos){
+
+	this->archivo.seekp(this->proximoEspacioLibre(),ios::beg);
+	if (atributos->size() != listaTipoAtributos->size()) {
+		//Tirar excepcion
+	}
+	list<Atributo>::iterator it2 = atributos->begin();
+	for (list<tamanioYTipoAtributo>::iterator it = listaTipoAtributos->begin(); it != listaTipoAtributos->end(); it++) {
+		if (it->tipo == TEXTO) {
+			this->archivo.write(it2->texto,it->cantidadBytes);
+		} else {
+			this->archivo.write((char*)&(it2->entero),it->cantidadBytes);
+		}
+		it2++;
+	}
 
 }
+ int ArchivoRegistroFijo::proximoEspacioLibre(){
+	 return 0;
+ }
