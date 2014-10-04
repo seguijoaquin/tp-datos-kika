e76f08841c686c27686ad9ade86622df4e37bdd9
franery10@gmail.com <franery10@gmail.com@60b1a8d6-6904-be9c-73d1-5d005d5b9d01>
2014-10-04 14:33:15 +0000

    Mas cosas base

---
+++ src/Capa Fisica/Archivos/Bloques/Bloques.h
@@ -0,0 +1,16 @@
+#ifndef ARCHIVO_BLOQUES
+#define ARCHIVO_BLOQUES
+
+#include "../Archivo.h"
+
+using namespace std;
+
+class ArchivoBloques: public Archivo {
+private:
+
+public:
+	ArchivoBloques();
+	~ArchivoBloques();
+};
+
+#endif
---
+++ src/Capa Fisica/Archivos/RegistroFijo/RegistroFijo.h
@@ -0,0 +1,16 @@
+#ifndef ARCHIVO_REGISTRO_FIJO
+#define ARCHIVO_REGISTRO_FIJO
+
+#include "../Archivo.h"
+
+using namespace std;
+
+class ArchivoRegistroFijo: public Archivo {
+private:
+
+public:
+	ArchivoRegistroFijo();
+	~ArchivoRegistroFijo();
+};
+
+#endif
