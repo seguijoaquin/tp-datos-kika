1b124d4b24f5e6aaeaa7aa28d68df291a46c900f
segui.joaquin <segui.joaquin@60b1a8d6-6904-be9c-73d1-5d005d5b9d01>
2014-11-15 15:33:47 +0000

    Corregidos subdir.mk

--- src/Capa Fisica/Excepciones/subdir.mk
+++ src/Capa Fisica/Excepciones/subdir.mk
@@ -4,26 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../CapaFisica/Excepciones/ExcepcionBloqueIncorrecto.cpp \
-../CapaFisica/Excepciones/ExcepcionBloqueInexistente.cpp \
-../CapaFisica/Excepciones/ExcepcionOverflowTamBloque.cpp \
-../CapaFisica/Excepciones/ExcepcionPosicionEnEspacioLibre.cpp 
+../Capa\ Fisica/Excepciones/ExcepcionBloqueIncorrecto.cpp \
+../Capa\ Fisica/Excepciones/ExcepcionBloqueInexistente.cpp \
+../Capa\ Fisica/Excepciones/ExcepcionOverflowTamBloque.cpp \
+../Capa\ Fisica/Excepciones/ExcepcionPosicionEnEspacioLibre.cpp 
 
 OBJS += \
-./CapaFisica/Excepciones/ExcepcionBloqueIncorrecto.o \
-./CapaFisica/Excepciones/ExcepcionBloqueInexistente.o \
-./CapaFisica/Excepciones/ExcepcionOverflowTamBloque.o \
-./CapaFisica/Excepciones/ExcepcionPosicionEnEspacioLibre.o 
+./Capa\ Fisica/Excepciones/ExcepcionBloqueIncorrecto.o \
+./Capa\ Fisica/Excepciones/ExcepcionBloqueInexistente.o \
+./Capa\ Fisica/Excepciones/ExcepcionOverflowTamBloque.o \
+./Capa\ Fisica/Excepciones/ExcepcionPosicionEnEspacioLibre.o 
 
 CPP_DEPS += \
-./CapaFisica/Excepciones/ExcepcionBloqueIncorrecto.d \
-./CapaFisica/Excepciones/ExcepcionBloqueInexistente.d \
-./CapaFisica/Excepciones/ExcepcionOverflowTamBloque.d \
-./CapaFisica/Excepciones/ExcepcionPosicionEnEspacioLibre.d 
+./Capa\ Fisica/Excepciones/ExcepcionBloqueIncorrecto.d \
+./Capa\ Fisica/Excepciones/ExcepcionBloqueInexistente.d \
+./Capa\ Fisica/Excepciones/ExcepcionOverflowTamBloque.d \
+./Capa\ Fisica/Excepciones/ExcepcionPosicionEnEspacioLibre.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-CapaFisica/Excepciones/%.o: ../CapaFisica/Excepciones/%.cpp
+Capa\ Fisica/Excepciones/%.o: ../Capa\ Fisica/Excepciones/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
 	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
