015a5bcc909664cdf177e0eb3d5791e824f1d949
segui.joaquin <segui.joaquin@60b1a8d6-6904-be9c-73d1-5d005d5b9d01>
2014-11-22 00:49:08 +0000

    Indices Secundarios

--- src/Capa Fisica/AdminIndicesSecundarios.cpp
+++ src/Capa Fisica/AdminIndicesSecundarios.cpp
@@ -10,22 +10,30 @@
 #include "../Capa Logica/ArbolBMas/ArbolBMas.h"
 
 AdministradorIndices::AdministradorIndices(){
-	this->arboles = new list<ArbolBMas>;
-	this->hashes = new list<Hash>;
-	this->rutaIndices = "\Indices Secundarios/";
-	//leer archivo con arboles y hash e hidratar
+	this->indices = new list<Indice>;
+	this->indicesCreados = fopen("indicesCreados.txt","r+b");
+	if(!this->indicesCreados)this->indicesCreados = fopen("indicesCreados.txt","w+");
+	this->inicializarIndices();
 }
 AdministradorIndices::~AdministradorIndices(){
+	list<Indice>::iterator it = this->indices->begin();
+	while (it != this->indices->end()){
+		if(it->tipo == ARBOL) delete it->arbol;
+		else delete it->hash;
+		++it;
+	}
+	fclose(this->indicesCreados);
+}
 
+void AdministradorIndices::inicializarIndices(){
+//Leer el archivo this->indicesCreados
+//Crear arboles y hash depende tipoIndice con nombreArchivo
+//Guardo arbol o hash en indice
+//Agrego indice a lista de indices
 }
 
 void AdministradorIndices::listar_indices(){
-	for (list<ArbolBMas>:: iterator it = this->arboles->begin(); it != this->arboles->end(); ++it) {
-		//listar nombres de arboles
-	}
-	for (list<Hash>::iterator it = this->hashes->begin(); it != this->hashes->end(); ++it) {
-		//listar nombres hashes
-	}
+
 
 }
 
@@ -33,48 +41,93 @@ void AdministradorIndices::eliminar_indice(int x){
 
 }
 
+void AdministradorIndices::persistirIndice(Indice* indice){
+	//[nombreArchivo tipoIndice nombreEntidad cantidadAtributos nombreAtributo+]
+	fprintf(this->indicesCreados,"%s ",StringUtil::stringToChar(indice->nombreArchivo));
+	fprintf(this->indicesCreados,"%i %s ", indice->tipo, StringUtil::stringToChar(indice->nombreEntidad));
+	fprintf(this->indicesCreados,"%zu ",(indice->atributos)->size());
+	list<metaDataAtributo>::iterator itAtt = indice->atributos->begin();
+	while(itAtt != indice->atributos->end()){
+		fprintf(this->indicesCreados,"%s ",StringUtil::stringToChar(itAtt->nombre));
+		++itAtt;
+	}
+	fprintf(this->indicesCreados,"\n");
+}
+
  void AdministradorIndices::crear_indice(Entidad* entidad){
+	 Indice indice;
+	 indice.nombreEntidad = entidad->getNombre();
 	 cout << "Ingrese el nombre del indice secundario: " << endl;
 	 string nombre;	 cin >> nombre;
+	 indice.nombreArchivo = nombre;
 	 cout << "Ingrese el tipo de estructura para el indice secundario:" << endl;
 	 cout << "	1). Arbol B+" << endl;
 	 cout << "	2). Hash" << endl;
-	 int tipo; cin >> tipo;
+	 int tipo, opc; cin >> tipo;
+	 indice.tipo = tipo;
 	 ArbolBMas* arbol = NULL;
 	 Hash* hash = NULL;
-	 if (tipo==1) {
-		 arbol = new ArbolBMas(this->rutaIndices+nombre);
-	 } else {
-		 hash = new Hash(this->rutaIndices+nombre,this->rutaIndices+nombre);
-	 }
+	 list<metaDataAtributo>* atts = new list<metaDataAtributo>;
+	 list<int>* numeroAtts = new list<int>;
+
+	 if (tipo==ARBOL)arbol = new ArbolBMas(nombre);
+	 else hash = new Hash(nombre,nombre);
+
 	 cout << "Ingrese los atributos que formaran parte del indice:" << endl;
 	 entidad->listarAtributos();
 	 int x; cin >> x;
-	 metaDataAtributo* atributo = entidad->getAtributo(x);
+	 atts->push_back(*entidad->getAtributo(x));
+	 numeroAtts->push_back(x);
+
 	 //PEDIR MAS ATRIBUTOS
-		 for (int i = 1; i <= entidad->getUltimoIDInstancia(); ++i) {
-			bool error;
-			Instancia* nuevaInstancia = entidad->getInstancia(i,error);
-			if (!error) {
-				Atributo* atributoInstancia = nuevaInstancia->getAtributo(x);
-				if (atributo->tipo == TEXTO) {
-					if (tipo==1) {
-						arbol->agregarValor(*(new Clave(atributoInstancia->texto)),StringUtil::int2string(i));
-					} else {
-						hash->insertarElemento(atributoInstancia->texto,StringUtil::int2string(i));
-					}
+	 cout << "Desea agregar otro atributo? 1.Si / 2.No: ";
+	 cin >> opc;
+	 while(opc == 1){
+		entidad->listarAtributos();
+		cin >> x;
+		atts->push_back(*entidad->getAtributo(x));
+		numeroAtts->push_back(x);
+		cout << "Desea agregar otro atributo? 1.Si / 2.No: ";
+		cin >> opc;
+	 }
+	 indice.atributos = atts;
+	 string claveStr = "";
+	 Atributo* atributoInstancia;
+	 for (int i = 1; i <= entidad->getUltimoIDInstancia(); ++i) {//i es el ID de la instancia
+		bool error;
+		Instancia* nuevaInstancia = entidad->getInstancia(i,error);
+		string claveStr = "";
+		if (!error) {
+			// CONCATENA CLAVES
+			list<metaDataAtributo>::iterator itAtt = atts->begin();
+			list<int>::iterator itNum = numeroAtts->begin();
+			while(itAtt != atts->end()){
+				atributoInstancia = nuevaInstancia->getAtributo(*itNum);
+				if (itAtt->tipo == TEXTO) {
+					claveStr += atributoInstancia->texto;
 				} else {
-					if (tipo==1){
-						arbol->agregarValor(*(new Clave(StringUtil::int2string(atributoInstancia->entero))),StringUtil::int2string(i));
-					} else {
-						hash->insertarElemento(StringUtil::int2string(atributoInstancia->entero),StringUtil::int2string(i));
-					}
+					claveStr += StringUtil::int2string(atributoInstancia->entero);
 				}
+				++itAtt;
+				++itNum;
 			}
-		 }
-	if (tipo == 1){
-		this->arboles->push_back(*arbol);
+			// AGREGA VALOR CON CLAVE CONCATENADA.
+			if (tipo==ARBOL) {
+				arbol->agregarValor(*(new Clave(claveStr)),StringUtil::int2string(i));
+			} else {
+				hash->insertarElemento(claveStr,StringUtil::int2string(i));
+			}
+		}
+	 }
+	if (tipo == ARBOL){
+		arbol->persistir();
+		indice.hash = NULL;
+		indice.arbol = arbol;
+		this->indices->push_back(indice);
 	} else {
-		this->hashes->push_back(*hash);
+		indice.arbol = NULL;
+		indice.hash = hash;
+		this->indices->push_back(indice);
 	}
+	this->persistirIndice(&indice);
 }
